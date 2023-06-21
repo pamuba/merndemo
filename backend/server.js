6de734dfe7353b927dabaf05fa0e0d5841b24975
kavyajadav <kjadhav@stevens.edu>
2023-06-21 20:05:38 +0530

    71

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config()
+const cors  = require('cors')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000
+
+//create the web server(expressJS app)
+const app = express();
+app.use(cors());
+app.use(express.json()); //use json parser
+
+
+//Connection code 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+// //http://localhost:300/exercises
+app.use('/exercises', exercisesRouter);
+// //http://localhost:300/users
+app.use('/users', usersRouter);
+
+
+//starts server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
